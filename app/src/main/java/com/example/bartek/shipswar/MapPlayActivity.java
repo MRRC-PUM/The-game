65997276bf51825ee37f66b4e87f40250865f139
mix1992 <mix.rybak85@gmail.com>
2015-05-12 21:44:27 +0200

    po awarii

--- app/src/main/java/com/example/bartek/shipswar/MapPlayActivity.java
+++ app/src/main/java/com/example/bartek/shipswar/MapPlayActivity.java
@@ -1,17 +1,23 @@
 package com.example.bartek.shipswar;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 
-public class MapPlayActivity extends ActionBarActivity {
+public class MapPlayActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_play);
+
     }
 
 
